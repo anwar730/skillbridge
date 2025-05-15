80374a8d4000a8a5f48f288dbd06b6b8fdb3b03b
anwarismail <anwaarismail449@gmail.com>
2025-05-15 15:02:09 +0300

    Add necessary gems and enable cors

--- skillbridge-api/config/initializers/cors.rb
+++ skillbridge-api/config/initializers/cors.rb
@@ -14,3 +14,13 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*' # Replace with frontend domain in production
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options],
+      expose: ['Authorization']
+  end
+end
